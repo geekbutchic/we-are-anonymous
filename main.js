cdd5d234f30129bf768a279a8ca9e83529141d9e
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2020-10-28 15:48:59 -0400

    continued with solutions

--- main.js
+++ main.js
@@ -2,75 +2,80 @@
  * YOUR CODE HERE *
  ******************/
 
-const onlyOdds = function(numbers) {
-  oddBalls = numbers.filter(function(number) {
+const onlyOdds = function (numbers) {
+  oddBalls = numbers.filter(function (number) {
     return (number % 2 === 1)
-  }) 
+  })
   return oddBalls;
 }
 
-const onlyEvens = function(numbers) {
-  evenSteven = numbers.filter(function(number) {
+const onlyEvens = function (numbers) {
+  evenSteven = numbers.filter(function (number) {
     return (number % 2 === 0)
   })
   return evenSteven;
 }
 
-const shortNamesOnly = function(names) {
-  whoYouCallinShort = names.filter(function(names) {
+const shortNamesOnly = function (names) {
+  whoYouCallinShort = names.filter(function (names) {
     return names.length < 7
   })
-return whoYouCallinShort;
+  return whoYouCallinShort;
 }
 
-const sNames = function(names) {
-  theChosenOnes = names.filter(function(name) {
+const sNames = function (names) {
+  theChosenOnes = names.filter(function (name) {
     return name.startsWith('S')
   })
   return theChosenOnes
 }
 
-const tripleAll = function(numbers) {
-  timesThree = numbers.map(function(number) {
+const tripleAll = function (numbers) {
+  timesThree = numbers.map(function (number) {
     return (number * 3)
   })
   return timesThree;
 }
 
-const negativeValues = function(numbers) {
-  debbieDowner = numbers.map(function(number) {
+const negativeValues = function (numbers) {
+  debbieDowner = numbers.map(function (number) {
     return -Math.abs(number) || 0
   })
   return debbieDowner;
 }
 
-const questionings = function(greetings) {
-  happyDays = greetings.map(function(greeting) {
+const questionings = function (greetings) {
+  happyDays = greetings.map(function (greeting) {
     return greeting + '?'
   })
   return happyDays;
 }
 
-const doubleEven = function(numbers) {
-  doubleTrouble = numbers.map(function(number) {
-    return -Map.abs(number / 2 === 0)
+const doubleEven = function (numbers) {
+  doubleTrouble = numbers.map(function (number) {
+    if (number % 2 === 0) {
+      return number * 2;
+    } else {
+      return number;
+    }
   })
   return doubleTrouble;
 }
 
-const changeToInitials = function(initials) {
-  signHereWith = initials.map(function(initial) {
-    return initial.startsWith
+const changeToInitials = function (initials) {
+  signHereWith = initials.map(function (initial) {
+    return initial.charAt(0).toUpperCase() + initial[i]
   })
-  return signHereWith;
+  return signHereWith
 }
 
-const titleCaseNames = function(names) {
-  crazyCase = names.map(function(name) {
-    return name.charAt(0).toUpperCase() + name.substr(1).toLowerCase();
+
+const titleCaseNames = function (names) {
+  crazyCase = names.map(function (name) {
+    return name.charAt(0).toUpperCase() + name.substr(1).toLowerCase()
   })
   return crazyCase;
-} 
+}
 
 
 
